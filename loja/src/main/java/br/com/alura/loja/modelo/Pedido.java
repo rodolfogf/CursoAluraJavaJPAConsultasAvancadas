971553784dd27db24a154584eaee792d5f63e7db
Rodolfo Faria <rodolfogf@gmail.com>
2023-01-17 22:35:54 -0300

    Aula 01 - Relacionamentos many-to-many

---
+++ loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,82 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date date;
+	private Long id_cliente;
+	private BigDecimal valorTotal;
+	private LocalDate data = LocalDate.now();
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	@OneToMany
+	private List<ItemPedido> items;
+	
+	public Pedido() {
+		
+	}
+
+	public Pedido(BigDecimal valorTotal) {
+		
+		this.valorTotal = valorTotal;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Long getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(Long id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+}
