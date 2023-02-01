82b0894fdfe84de0d2781c6e80271fb25c359c85
Rodolfo Faria <rodolfogf@gmail.com>
2023-01-31 22:45:27 -0300

    Aula 02 - Consultas com select new

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
-
 import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ClienteDao;
 import br.com.alura.loja.dao.PedidoDao;
@@ -16,6 +15,7 @@ import br.com.alura.loja.modelo.ItemPedido;
 import br.com.alura.loja.modelo.Pedido;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 public class CadastroDePedido {
 
@@ -24,36 +24,46 @@ public class CadastroDePedido {
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		Produto p1 = produtoDao.buscarPorId(1l);
+		Produto p2 = produtoDao.buscarPorId(2l);
+		Produto p3 = produtoDao.buscarPorId(3l);
 		ClienteDao clienteDao = new ClienteDao(em);
 		Cliente cl1 = clienteDao.buscarPorId(1l);
 		
 		em.getTransaction().begin();		
 		
 		Pedido pd1 = new Pedido(cl1);		
-		pd1.adicionarItem(new ItemPedido(10, pd1, p1));
+		pd1.adicionarItem(new ItemPedido(10, pd1, p1));		
+		pd1.adicionarItem(new ItemPedido(40, pd1, p2));
+		
+		Pedido pd2 = new Pedido(cl1);		
+		pd1.adicionarItem(new ItemPedido(2, pd2, p3));
 		
 		PedidoDao pedidoDao = new PedidoDao(em);
-		pedidoDao.cadastrar(pd1);		
+		pedidoDao.cadastrar(pd1);
+		pedidoDao.cadastrar(pd2);
 
 		em.getTransaction().commit();
 		
 		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
 		System.out.println("Valor Total: " + totalVendido);
 		
-		List<Object[]> relatorio = pedidoDao.relatorioDeVendas();
-		for (Object[] obj : relatorio) {
-			System.out.println(obj[0]);
-			System.out.println(obj[1]);
-			System.out.println(obj[2]);
-		}		
+		List<RelatorioDeVendasVo> relatorio = pedidoDao.relatorioDeVendas();
+		relatorio.forEach(System.out::println);
 	}
 
 	/*Código copiado da classe CadastroDeProduto apenas para ganhar tempo no teste 
 	e por estar sendo usado um bando de dados em memória*/
 	private static void popularBancoDeDados() {
 		Categoria c1 = new Categoria("celulares");
+		Categoria c2 = new Categoria("videogames"); 
+		Categoria c3 = new Categoria("informatica");		
+		
 		Produto p1 = new Produto(
 				"Xiaomi Redmi", "8 Gb RAM, 64 Gb",  new BigDecimal("800"), c1);
+		Produto p2 = new Produto(
+				"PS5", "Playstation 5", new BigDecimal("5500"), c2);
+		Produto p3 = new Produto(
+				"Macbook", "Macbook top", new BigDecimal("8500"), c3);
 		
 		Cliente cl1 = new Cliente("ClienteTeste", "123456");
 		
@@ -65,7 +75,11 @@ public class CadastroDePedido {
 		
 		em.getTransaction().begin();
 		categoriaDao.cadastrar(c1);
+		categoriaDao.cadastrar(c2);
+		categoriaDao.cadastrar(c3);
 		produtoDao.cadastrar(p1);
+		produtoDao.cadastrar(p2);
+		produtoDao.cadastrar(p3);
 		clienteDao.cadastrar(cl1);
 		em.getTransaction().commit();
 		em.close();
